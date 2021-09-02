379bc81310d5298ddaebefc13a93b964ba77e1ed
Loic Denuziere <ldenuziere@d-edge.com>
2021-09-02 16:59:37 +0200

    Add C# custom differ test

---
+++ tests/Diffract.CSharp.Tests/CustomDiffers.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.FSharp.Core;
+using TypeShape.Core;
+using Xunit;
+
+namespace Diffract.CSharp.Tests
+{
+    public class CustomDiffers
+    {
+        [Fact]
+        public void NoCustomDiffer()
+        {
+            var expectedDiff = "D.X Expect = \"a\"\n    Actual = \"b\"\n";
+            var expected = new Container(new CustomDiffable("a"));
+            var actual = new Container(new CustomDiffable("b"));
+            var actualDiff = Diffract.ToString(expected, actual);
+            Assert.Equal(expectedDiff, actualDiff);
+        }
+
+        [Fact]
+        public void CustomDiffer()
+        {
+            var expectedDiff = "D Expect = \"a\"\n  Actual = \"b\"\n";
+            var expected = new Container(new CustomDiffable("a"));
+            var actual = new Container(new CustomDiffable("b"));
+            var actualDiff = MyDiffer.Get<Container>().ToString(expected, actual);
+            Assert.Equal(expectedDiff, actualDiff);
+        }
+
+        public record CustomDiffable(string X);
+
+        public record Container(CustomDiffable D);
+
+        public class MyDiffer : IDiffer<CustomDiffable>
+        {
+            private readonly IDiffer<string> _stringDiffer;
+
+            public MyDiffer(IDifferFactory differFactory)
+            {
+                _stringDiffer = differFactory.GetDiffer<string>();
+            }
+
+            public FSharpOption<Diff> Diff(CustomDiffable x1, CustomDiffable x2) =>
+                _stringDiffer.Diff(x1.X, x2.X);
+
+            public static IDiffer<T> Get<T>() => Singleton<T>.Instance;
+
+            private static class Singleton<T>
+            {
+                public static readonly IDiffer<T> Instance = new CustomDiffer().GetDiffer<T>();
+            }
+
+            private class CustomDiffer : ICustomDiffer
+            {
+                public FSharpOption<IDiffer<T>> GetCustomDiffer<T>(IDifferFactory differFactory, Core.TypeShape<T> shape) =>
+                    shape.Type == typeof(CustomDiffable)
+                        ? new MyDiffer(differFactory).Unwrap<CustomDiffable, T>()
+                        : null;
+            }
+        }
+    }
+}
